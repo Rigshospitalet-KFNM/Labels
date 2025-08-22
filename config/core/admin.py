4ff629c76a65eb73521a253c428f1b9fb82653c0
Mathias Bardram Johnbeck <Mathiasjohnbeck@gmail.com>
2025-08-22 12:47:55 +0200

    Some models

--- config/core/admin.py
+++ config/core/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.http import HttpRequest
+from .models import Signatory, Element, LabelTemplate
 
-# Register your models here.
+#admin.site.register(model here)
+admin.site.register(Signatory)
+
+admin.site.register(Element)
+
+@admin.register(LabelTemplate)
+class templateAdmin(admin.ModelAdmin):
+    list_display = ()
+
+    #adding and editing done through custom page
+    def has_add_permission(self, request):
+        return False
+    
+    def has_change_permission(self, request, obj=None):
+        return False
+    
+    #only allow delete from admin page
+    def has_delete_permission(self, request, obj=None):
+        return True
--- config/core/models.py
+++ config/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Signatory(models.Model):
+    name = models.CharField(max_length=100)
+    initials = models.CharField(max_length=100)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+class Element(models.Model):
+    code = models.CharField(max_length=50)
+    radioactive = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.code
+    
+class LabelTemplate(models.Model):
+    def __str__(self) -> str:
+        return super().__str__()
