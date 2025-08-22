b4a62c74106945c1b4a3ef66ed21c9277e9f43e4
Mathias Bardram Johnbeck <Mathiasjohnbeck@gmail.com>
2025-08-22 13:23:27 +0200

    LabelSetttings

--- config/core/models.py
+++ config/core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import ValidationError
 
 class Signatory(models.Model):
     name = models.CharField(max_length=100)
@@ -17,3 +18,23 @@ class Element(models.Model):
 class LabelTemplate(models.Model):
     def __str__(self) -> str:
         return super().__str__()
+    
+
+class LabelSettings(models.Model):
+    width = models.FloatField(default=63.5,help_text="Label width in mm")
+    height = models.FloatField(default=33.9,help_text="Label height in mm")
+
+
+    def save(self, *args, **kwargs):
+        if not self.pk and LabelSettings.objects.exists():
+            raise ValidationError("Only one LabelSettings instance is allowed.")
+        return super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f"{self.width} x {self.height} mm"
+    
+    @classmethod
+    def get_solo(cls):
+        """Always return the one settings instance, create it if missing"""
+        obj, created = cls.objects.get_or_create(pk=1)
+        return obj
